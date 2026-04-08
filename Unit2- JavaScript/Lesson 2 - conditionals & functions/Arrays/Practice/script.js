e11f6193249e7d60e093833514b9235e95c4f909
vicksidhu <vick.sidhu009@gmail.com>
2026-04-07 21:00:18 -0700

    Practice changes

--- Unit2- JavaScript/Lesson 2 - conditionals & functions/Arrays/Practice/script.js
+++ Unit2- JavaScript/Lesson 2 - conditionals & functions/Arrays/Practice/script.js
@@ -1,29 +1,38 @@
 // TODO 1: Create an array named "colors" with at least 5 color names (e.g., "red", "blue", "green", etc.)
 
+let colors = ["red", "blue", "green", "yellow", "purple"];
 
 
 // TODO 2: Log the array to the console
-
+console.log(colors);
 
 // TODO 3: Log the length of the array using the length property
-
+console.log(colors.length); // 5
 
 // TODO 4: Add a new color to the end of the array using the push method
-
+colors.push("orange");
+console.log(colors);
 
 // TODO 5: Remove the last element of the array using the pop method and log the removed color
+const removedColor = colors.pop();
+console.log(removedColor);
 
 // TODO 6: Remove the first element of the array using the shift method and log the removed color
-
+const removedFirstColor = colors.shift();
+console.log(removedFirstColor);
 
 // TODO 7: Add a new color to the beginning of the array using the unshift method
-
+colors.unshift("pink");
+console.log(colors);
 
 // TODO 8: Create a new array called "selectedColors" using the slice method to include the first 2 colors
-
+const selectedColors = colors.slice(0, 2);
+console.log(selectedColors);
 
 // TODO 9: Use the splice method to remove 2 colors starting from the 1st index and add "black" and "white" instead
-
+colors.splice(1, 2, "black", "white");
+console.log(colors);
 
 // TODO 10: Log the final array and its length
-
+console.log(colors);
+console.log(colors.length);
--- Unit2- JavaScript/Lesson 3 - Loops/Basic loops/Practice/script.js
+++ Unit2- JavaScript/Lesson 3 - Loops/Basic loops/Practice/script.js
@@ -1,10 +1,25 @@
 // TODO 1: Create an array called 'numbers' with 5 random numbers
-
+const numbers = [1, 2, 3, 4, 5];
 
 // TODO 2: Use a 'for' loop to log each number in the array to the console
-
+for (let i = 0; i < numbers.length; i++) {
+    console.log(numbers[i]);
+}   
 
 // TODO 3: Use a 'while' loop to log numbers less than 5 from the array
-
+let index = 0;
+while (index < numbers.length) {
+    if (numbers[index] < 5) {
+        console.log(numbers[index]);
+    }
+    index++;
+}
 
 // TODO 4: Use a 'do...while' loop to ask the user for a number between 1 and 3
+
+let userNumber;
+do {
+    userNumber = parseInt(prompt("Please enter a number between 1 and 3:"));
+} while (userNumber < 1 || userNumber > 3);
+console.log("You entered:", userNumber);
+
