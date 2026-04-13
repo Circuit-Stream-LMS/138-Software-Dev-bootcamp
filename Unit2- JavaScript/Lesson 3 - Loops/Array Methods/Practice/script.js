de05ee61dbe08daa3f9bedc0a1d97383c4fdcfb2
vicksidhu <vick.sidhu009@gmail.com>
2026-04-12 17:02:27 -0700

    Notes from class

--- Unit2- JavaScript/Lesson 3 - Loops/Array Methods/Practice/script.js
+++ Unit2- JavaScript/Lesson 3 - Loops/Array Methods/Practice/script.js
@@ -2,16 +2,23 @@
 const temperatures = [72, 68, 75, 79, 85, 91, 73, 77, 69, 82];
 
 // TODO 1: Use forEach to log each temperature in Fahrenheit and its Celsius equivalent
+temperatures.forEach(temp => {
+    const celsius = (temp - 32) * 5/9;
+    console.log(`Temp: ${temp}°F, Celsius: ${celsius.toFixed(0)}°C`);
+});
 
 
 // TODO 2: Use map to create a new array where each temperature is increased by 5 degrees
-
+const increasedTemperatures = temperatures.map(temp => temp + 5);
+console.log(increasedTemperatures);
 
 // TODO 3: Use filter to create a new array with temperatures above 80°F
-
+const highTemperatures = temperatures.filter(temp => temp > 80);
+console.log(highTemperatures);
 
 // TODO 4: Use reduce to find the average temperature
-
+const averageTemperature = temperatures.reduce((sum, temp) => sum + temp, 0) / temperatures.length
+console.log(averageTemperature);
 
 // Additional Practice Examples
 
